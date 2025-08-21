1abc34d41a8a949cf6b2d6c2bb1c0fd24c00a340
Dan Whitman <dwhitman44@gmail.com>
2025-08-21 14:50:27 -0400

    Transfer.

--- boards/pygamer/examples/usb_poll.rs
+++ boards/pygamer/examples/usb_poll.rs
@@ -4,21 +4,27 @@
 #![no_std]
 #![no_main]
 
-use bsp::{entry, hal, pac, Pins, RedLed};
+use core::cell::OnceCell;
+use cortex_m::asm::delay as cycle_delay;
+use cortex_m::peripheral::NVIC;
 #[cfg(not(feature = "panic_led"))]
 use panic_halt as _;
-use pygamer as bsp;
-
-use hal::clock::GenericClockController;
-use hal::prelude::*;
-use pac::{CorePeripherals, Peripherals};
+use usb_device::bus::UsbBusAllocator;
 use usb_device::prelude::*;
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+use pygamer as bsp;
+
+use bsp::{entry, hal, pac, Pins, RedLed};
+use hal::clock::GenericClockController;
+use hal::prelude::*;
+use hal::usb::UsbBus;
+use pac::{interrupt, CorePeripherals, Peripherals};
 
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-    let mut _core = CorePeripherals::take().unwrap();
+    let mut core = CorePeripherals::take().unwrap();
     let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.gclk,
         &mut peripherals.mclk,
@@ -26,63 +32,70 @@ fn main() -> ! {
         &mut peripherals.oscctrl,
         &mut peripherals.nvmctrl,
     );
-
     let pins = Pins::new(peripherals.port).split();
+    let mut red_led: bsp::RedLed = pins.led_pin.into();
 
-    let usb_bus = pins
-        .usb
-        .init(peripherals.usb, &mut clocks, &mut peripherals.mclk);
+    let bus_allocator = unsafe {
+        let _ = USB_ALLOCATOR.set(pins.usb.init(
+            peripherals.usb,
+            &mut clocks,
+            &mut peripherals.mclk,
+        ));
+        USB_ALLOCATOR.get().unwrap()
+    };
 
-    let mut serial = SerialPort::new(&usb_bus);
-    let mut led: RedLed = pins.led_pin.into();
-
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .strings(&[StringDescriptors::new(LangID::EN)
-            .manufacturer("Fake company")
-            .product("Serial port")
-            .serial_number("TEST")])
-        .expect("Failed to set strings")
-        .device_class(USB_CLASS_CDC)
-        .build();
-
-    let mut led_state = false;
-    let _ = led.set_low(); // Turn off
-
-    loop {
-        if !usb_dev.poll(&mut [&mut serial]) {
-            continue;
-        }
-
-        let mut buf = [0u8; 64];
-
-        match serial.read(&mut buf) {
-            Ok(count) if count > 0 => {
-                // Echo back in upper case
-                for c in buf[0..count].iter_mut() {
-                    led_state = if led_state {
-                        let _ = led.set_low(); // Turn off
-                        false
-                    } else {
-                        let _ = led.set_high(); // Turn on
-                        true
-                    };
-
-                    if 0x61 <= *c && *c <= 0x7a {
-                        *c &= !0x20;
-                    }
-                }
-
-                let mut write_offset = 0;
-                while write_offset < count {
-                    match serial.write(&buf[write_offset..count]) {
-                        Ok(len) if len > 0 => {
-                            write_offset += len;
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            _ => {}
-        }
+    unsafe {
+        let _ = USB_SERIAL.set(SerialPort::new(bus_allocator));
+        let _ = USB_BUS.set(
+            UsbDeviceBuilder::new(bus_allocator, UsbVidPid(0x16c0, 0x27dd))
+                .strings(&[StringDescriptors::new(LangID::EN)
+                    .manufacturer("Fake company")
+                    .product("Serial port")
+                    .serial_number("TEST")])
+                .expect("Failed to set strings")
+                .device_class(USB_CLASS_CDC)
+                .build(),
+        );
     }
+
+    unsafe {
+        core.NVIC.set_priority(interrupt::USB, 1);
+        NVIC::unmask(interrupt::USB);
+    }
+
+    // Flash the LED in a spin loop to demonstrate that USB is
+    // entirely interrupt driven.
+    loop {
+        cycle_delay(15 * 1024 * 1024);
+        red_led.toggle().ok();
+    }
+}
+
+static mut USB_ALLOCATOR: OnceCell<UsbBusAllocator<UsbBus>> = OnceCell::new();
+static mut USB_BUS: OnceCell<UsbDevice<UsbBus>> = OnceCell::new();
+static mut USB_SERIAL: OnceCell<SerialPort<UsbBus>> = OnceCell::new();
+
+fn poll_usb() {
+    unsafe {
+        if let Some(usb_bus) = USB_BUS.get_mut() {
+            if let Some(serial) = USB_SERIAL.get_mut() {
+                usb_bus.poll(&mut [serial]);
+                let mut buf = [0u8; 64];
+
+                if let Ok(count) = serial.read(&mut buf) {
+                    for (i, c) in buf.iter().enumerate() {
+                        if i >= count {
+                            break;
+                        }
+                        serial.write(&[*c]).ok();
+                    }
+                };
+            };
+        }
+    };
+}
+
+#[interrupt]
+fn USB() {
+    poll_usb();
 }
